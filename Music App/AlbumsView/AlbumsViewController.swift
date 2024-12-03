c5c761af0adeb828ff35598839c1832446560cab
Mauricio Aguilera <artuaguila@gmail.com>
2024-12-03 17:45:07 -0600

    Updating Albums View to the new music data struct

--- Music App/AlbumsView/AlbumsViewController.swift
+++ Music App/AlbumsView/AlbumsViewController.swift
@@ -13,12 +13,11 @@ class AlbumsViewController: UIViewController {
     @IBOutlet weak var bandLabel : UILabel!
     var dataSource: UITableViewDiffableDataSource<Int,String>?
     
-    var albumsData: [String] = []
-    var bandName: String = ""
+    var band: Band?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        bandLabel.text = bandName
+        bandLabel.text = band?.name
         setDataSource()
         setSnapshot()
         tableView.delegate = self
@@ -37,29 +36,19 @@ class AlbumsViewController: UIViewController {
     func setSnapshot(){
         var snapshot = NSDiffableDataSourceSnapshot<Int,String>()
         snapshot.appendSections([0])
-//        var albums : [String] = []
-//        for album in albumsData{
-//            if let nombre = album["nombre"] as? String,
-//                   let canciones = album["canciones"] as? [String] {
-//                    
-//                    // Imprimimos el nombre del álbum
-//                    print("Álbum: \(nombre)")
-//                    
-//                    // Iteramos sobre las canciones de este álbum
-//                    for canción in canciones {
-//                        print("  Canción: \(canción)")
-//                    }
-//                }
-//        }
-        snapshot.appendItems([])
+        if let alumbs = band?.albums.map({$0.name}){
+            snapshot.appendItems(alumbs)
+        }
         dataSource?.apply(snapshot)
     }
 }
 
 extension AlbumsViewController : UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       // var songsViewController = SongsViewController(nibName: nil, bundle: nil)
-        ///*songsViewController*/.albums = musicData[indexPath.row]
+        var songsViewController = SongsViewController(nibName: nil, bundle: nil)
+        songsViewController.album = band?.albums[indexPath.row]
+        songsViewController.modalPresentationStyle = .fullScreen
+        present(songsViewController, animated: true, completion: nil)
         
     }
     
---
+++ Music App/SongsView/SongTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SongTableViewCell.swift
+//  Music App
+//
+//  Created by Diplomado on 03/12/24.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
